b5e83adf45dbe1558d3051126d2bee8d690ff28a
DevLucasOliveira <lucasoliveira.si@outlook.com>
2021-07-15 08:28:27 -0300

    first commit

---
+++ WorkShop/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WorkShop
+{
+    public class Class1
+    {
+    }
+}
---
+++ WorkShopTestes/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace WorkShopTestes
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
